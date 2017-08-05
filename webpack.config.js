9c2d315b90c3a828e13c6f37ab6d4e3e1c4f5bd6
triestpa <patrick.triest@gmail.com>
2017-08-05 13:46:46 +0700

    Add js minification to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,35 @@
+const path = require("path");
+const webpack = require("webpack");
+const BabiliPlugin = require("babili-webpack-plugin");
+
 module.exports = {
-    entry: "./app/app.js",
+    entry: "./src/app.js",
     output: {
-        path: __dirname,
-        filename: "./public/bundle.js"
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js"
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
+            { test: /\.css$/, loader: "style!css" },
+            {
+                loader: "babel-loader",
+
+                // Skip any files outside of your project's `src` directory
+                include: [
+                    path.resolve(__dirname, "src"),
+                ],
+
+                // Only run `.js` and `.jsx` files through Babel
+                test: /\.js$/,
+
+                // Options to configure babel with
+                query: {
+                    presets: ['es2016'],
+                }
+            },
         ]
     },
+    plugins: [
+        new BabiliPlugin({})
+    ]
 };
