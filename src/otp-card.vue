8441f11f05c2a4ddb8a6f16b79df9c90d8a33879
triestpa <patrick.triest@gmail.com>
2017-08-06 15:58:21 +0700

    Simplifiy OTP card layout

--- src/otp-card.vue
+++ src/otp-card.vue
@@ -1,36 +1,15 @@
 <template>
-  <div>
-    <section class="section">
-      <div class="container">
-        <div class="card">
-          <header class="card-header">
-            <p class="card-header-title">
-              Component
-            </p>
-            <a class="card-header-icon">
-              <span class="icon">
-                <i class="fa fa-angle-down"></i>
-              </span>
-            </a>
-          </header>
-          <div class="card-content has-text-centered">
-            <qr-code></qr-code>
-            <div class="content">
-              <h1>{{ otp }}</h1>
-              <small>{{ time }}</small>
-              <h3 id="error"></h3>
-              <progress class="progress is-info" value="45" max="100">45%</progress>
-            </div>
-          </div>
-          <footer class="card-footer">
-            <a class="card-footer-item">Share</a>
-            <a class="card-footer-item">Edit</a>
-            <a class="card-footer-item">Delete</a>
-          </footer>
-        </div>
-      </div>
-    </section>
-  </div>
+  <section class="otp-card">
+    <p>{{ name }}</p>
+    <p>{{ secret }}</p>
+    <qr-code :content="qrContent"></qr-code>
+    <div class="content">
+      <h1>{{ otp }}</h1>
+      <small>{{ time }}</small>
+      <h3 id="error"></h3>
+      <progress class="progress is-info" value="45" max="100">45%</progress>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -40,28 +19,35 @@ import qrCode from './qr-code.vue'
 export default {
   name: 'otp-card',
   components: { qrCode },
+  props: {
+    name: String,
+    secret: String
+  },
   data () {
     return {
       secret: null,
       time: null,
       otp: null,
       generator: null,
+      qrContent: null,
     }
   },
 
   created () {
     // Use var for these variables, since we want them in the global scope.
-    let newSecret = OTP.getRandomInt(0, Math.pow(10, 12))
+    //let newSecret = OTP.getRandomInt(0, Math.pow(10, 12))
 
     // Show default generated secret value in UI
-    this.setSecret(newSecret)
+    this.setSecret(this.secret)
+
+    this.time = new Date().toLocaleString()
+    this.otp = this.generator.getTOTP()
   },
 
   mounted () {
    setInterval(() => {
         // Update time and value every second
-        let date = new Date()
-        this.time = date.toLocaleString()
+        this.time = new Date().toLocaleString()
         this.otp = this.generator.getTOTP()
     }, 1000)
   },
@@ -74,15 +60,10 @@ export default {
 
     setQRCode (label = 'test') {
       let secretb32 =  this.generator.getBase32Secret()
-      let qrContent = `otpauth://totp/${label}?secret=${secretb32}`
+      this.qrContent = `otpauth://totp/${label}?secret=${secretb32}`
     },
 
     setSecret (newSecret) {
-      // let secretInput = document.getElementById('secret')
-      // if (!newSecret) {
-      //   newSecret = secretInput.value
-      // }
-
       newSecret = String(newSecret)
 
       try {
@@ -98,8 +79,14 @@ export default {
 
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
-
+.otp-card {
+  padding: 24px;
+  border: 1px solid black;
+  border-radius: 5px;
+  text-align: center;
+  margin: 24px;
+}
 
 </style>
