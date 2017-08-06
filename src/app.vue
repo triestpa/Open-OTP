ca628b225f12fe8a60f763c468ee46db48ab87ee
triestpa <patrick.triest@gmail.com>
2017-08-06 15:58:37 +0700

    Add encryption utilities

--- src/app.vue
+++ src/app.vue
@@ -1,26 +1,67 @@
 <template>
   <div id="app">
-    <otp-card></otp-card>
-    <otp-card></otp-card>
+    <div class="otp-card-list">
+      <div class="otp-card-container" v-for="(otp, index) in otps" :key="index">
+        <otp-card :name="otp.name" :secret="otp.secret"></otp-card>
+      </div>
+    </div>
+    <a href="distribution/" target="_blank" rel="noopener">Test Psuedorandom Number Generator Distribution.</a>
   </div>
 </template>
 
 <script>
-import OtpCard from './otp-card.vue'
+import AES from 'crypto-js/aes'
+import utf8 from 'crypto-js/enc-utf8';
+import otpCard from './otp-card.vue'
 
 export default {
   name: 'app',
-  components: { OtpCard },
+  components: { otpCard },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      otps: []
+    }
+  },
+  created () {
+    const sampleOTP = {
+      name: 'Sample OTP',
+      secret: '123456'
+    }
+
+    this.otps.push(sampleOTP)
+    this.encrypt('test123')
+  },
+  methods: {
+    // Encrypt stored keys with AES-256 encryption
+    encrypt (encryptionKey) {
+      let encrypted = AES.encrypt(JSON.stringify(this.otps), encryptionKey)
+      let decryptedBytes = AES.decrypt(encrypted.toString(), encryptionKey)
+      let decryptedText = decryptedBytes.toString(utf8)
+      let decryptedObj = JSON.parse(decryptedText)
+      console.log(decryptedObj)
+    },
+    bytesToString(bytes) {
+      let result = ''
+      for (let i = 0; i < bytes.length; i++) {
+        result += String.fromCharCode(parseInt(bytes[i], 2));
+      }
+      return result;
     }
   }
 }
 </script>
 
 <style lang="scss">
+@import "~normalize.css/normalize.css";
+@import "_variables.scss";
 
+.otp-card-list {
+  margin: 24px;
+}
 
+#app {
+
+}
 
 </style>
