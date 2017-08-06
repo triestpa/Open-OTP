0e89b8b411d5842e28153915490a2283b45fd937
triestpa <patrick.triest@gmail.com>
2017-08-06 15:56:49 +0700

    Add more advanced CSS processing

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,37 @@ const webpack = require("webpack");
 const BabiliPlugin = require("babili-webpack-plugin");
 
 module.exports = {
-    entry: "./src/main.js",
-    output: {
-        path: path.resolve(__dirname, "public"),
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    loaders: {
-                        'scss': 'vue-style-loader!css-loader!sass-loader',
-                        'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
-                    }
-                }
-            },
-            {
-                loader: "babel-loader",
-                include: [ path.resolve(__dirname, "src") ],
-                test: /\.js$/,
-                query: { presets: ['es2017'] }
-            }
-        ]
-    },
-    plugins: [
-        new BabiliPlugin({}),
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('production')
-            }
-        })
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [{
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            'scss': 'vue-style-loader!css-loader?minimize&{discardComments:{removeAll:true}}!sass-loader',
+          }
+        }
+      },
+      {
+        loader: "babel-loader",
+        include: [path.resolve(__dirname, "src")],
+        test: /\.js$/,
+        query: {
+          presets: ['es2017']
+        }
+      }
     ]
-};
+  },
+  plugins: [
+    new BabiliPlugin({}),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    })
+  ]
+}
